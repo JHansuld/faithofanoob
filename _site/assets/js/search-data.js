c3426cc3d1d9e4a6cd4c7fe6597ab6bca807417e
John Hansuld <jehansuld@gmail.com>
2022-02-02 18:50:51 -0600

    Finished the design on the blog and created blog posts

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "The Beginning",
+        "excerpt":"This is my First Post! I have begun the journey into tech, specifically into web development. Many have taken this journey and I have found numerous blogs, podcosts and videos that talk about these journeys. One thing that I noticed with these stories is that most of them had some...","categories": ["Blog Info"],
+        "tags": ["Journal","First Post","Bio"],
+        "url": "http://localhost:4000/blog%20info/beginning/"
+      },{
+        "title": "100 Days of Code Challenge - Day Five",
+        "excerpt":"Today I got a good 4 hours in. Mostly researching. I learned how to install ruby and Jekyll on my laptop. Then I went through the process of setting up my blog with Jekyll. I still do not know all of it but I am learning. Jekyll allows for the...","categories": ["100 days of code"],
+        "tags": ["Journal","Day 5"],
+        "url": "http://localhost:4000/100%20days%20of%20code/dayfive/"
+      },{
+        "title": "100 Days of Code Challenge: Day Six",
+        "excerpt":"This day I spent building the homepage css. It took me a while. I also did it 3 times and got frustrated. So I took a walk with Alphonse. I decided that I am going to start off with a theme. I need to not focus on the details as...","categories": ["100 days of code"],
+        "tags": ["Journal","Day 6","Jekyll","Encouragement","Study Tips"],
+        "url": "http://localhost:4000/100%20days%20of%20code/daysix/"
+      },{
+        "title": "Github Pages",
+        "excerpt":"This guide will show you how to set up Github Pages for your own website or blog. Github is an extremely useful site for storing repositories of code. You can use it to showcase your project, or to collaborate on a project with others. Github Pages will create a repository...","categories": ["Github"],
+        "tags": ["Github","Hosting","Guide","Blogging"],
+        "url": "http://localhost:4000/github/githubpages/"
+      },{
+        "title": "100 Days of Code Challenge: Day Seven",
+        "excerpt":"Today I focused on getting my blog finished. It took a while but I needed a slow day. Work canceled so I had the day off and used it to relax and get back in the right headspace. Alphonse was a huge fan of this idea and extremely supportive. I...","categories": ["100 days of code"],
+        "tags": ["Journal","Day 7","Jekyll","Encouragement","Study Tips","The Odin Project","Udemy","Learn to Code with Me","Web Developer"],
+        "url": "http://localhost:4000/100%20days%20of%20code/dayseven/"
+      },{
+        "title": "Installing Jekyll",
+        "excerpt":"This is my guide on how to install Jekyll to use with Github Pages. I am going to go over how to do this in linux command line with links to the download files as well. All info can be found at jekyllrb.com. First we have to install some prerequsites....","categories": ["Jekyll"],
+        "tags": ["Github","Jekyll","Hosting","Guide","Blogging","Ruby","RubyGems"],
+        "url": "http://localhost:4000/jekyll/installingjekyll/"
+      }]
